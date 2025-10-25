8fc77db315103dc50c22afd8fb232eb6f29468e1
blank-hoodie <d.m-2006@mail.ru>
2025-10-25 15:07:32 +0300

    version 3.0

--- pract_1/main_2.cpp
+++ pract_1/main_2.cpp
@@ -4,15 +4,18 @@ bool Pyth(unsigned a, unsigned b, unsigned c);
 int main() {
 	using ut = unsigned;
 	ut a = 0, b = 0, c = 0;
+	std::cin >> c >> b;
 	size_t count=0;
-	while (std::cin >> a && ) {
+	while (std::cin >> a) {
 		count+= Pyth(a, b, c)? 1 : 0;
+		c = b;
+		b = a;
 	}
 	if (std::cin.eof()) {
 		std::cout << count;
 		std::cout << "\n";
 	} else if (std::cin.fail()) {
-		std::cerr << "input err";
+		std::cerr << "input err\n";
 		return 1;
 	}
 }
@@ -22,3 +25,4 @@ bool Pyth(unsigned a, unsigned b, unsigned c) {
 	p = p || (a*a)==(b*b)+(c*c);
 	p = p || (b*b)+(a*a)==(c*c);
 	return p;
+}
