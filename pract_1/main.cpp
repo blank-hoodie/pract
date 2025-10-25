c69206259c0fccab5adb15de60f64fa16b3306db
blank-hoodie <d.m-2006@mail.ru>
2025-10-25 22:09:32 +0300

    end class

--- pract_1/main.cpp
+++ pract_1/main.cpp
@@ -2,23 +2,28 @@
 bool Pyth(unsigned a, unsigned b, unsigned c);
 
 int main() {
-	using ut = unsigned;
-	ut a = 0, b = 0, c = 0;
-	size_t count=0;
-	while (std::cin >> a && ) {
-		count+= Pyth(a, b, c)? 1 : 0;
-	}
-	if (std::cin.eof()) {
-		std::cout << count;
-		std::cout << "\n";
-	} else if (std::cin.fail()) {
-		std::cerr << "input err";
-		return 1;
-	}
+        using ut = unsigned;
+        ut a = 0, b = 0, c = 0;
+        std::cin >> c >> b;
+        size_t count=0;
+        while (std::cin >> a) {
+                count+= Pyth(a, b, c)? 1 : 0;
+                c = b;
+                b = a;
+        }
+        if (std::cin.eof()) {
+                std::cout << count;
+                std::cout << "\n";
+        } else if (std::cin.fail()) {
+                std::cerr << "input err\n";
+                return 1;
+        }
 }
 
 bool Pyth(unsigned a, unsigned b, unsigned c) {
-	bool p = (a*a)+(b*b)==(c*c);
-	p = p || (a*a)==(b*b)+(c*c);
-	p = p || (b*b)+(a*a)==(c*c);
-	return p;
+        bool p = (a*a)+(b*b)==(c*c);
+        p = p || (a*a)==(b*b)+(c*c);
+        p = p || (b*b)+(a*a)==(c*c);
+        return p;
+}
+
