338de97bdef7db485b7cb6f5e16a30783ac6e241
blank-hoodie <d.m-2006@mail.ru>
2025-10-25 14:56:26 +0300

    version 2.0

---
+++ pract_1/main_1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+	using ut = unsigned;
+	ut a = 0;
+	size_t count=0;
+	while (std::cin >> a) {
+		count+= a*a;
+	}
+	if (std::cin.eof()) {
+		std::cout << count;
+		std::cout << "\n";
+	} else if (std::cin.fail()) {
+		std::cerr << "input err";
+		return 1;
+	}
+}
