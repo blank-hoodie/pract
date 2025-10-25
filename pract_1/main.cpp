3e2173fc5a47324dc4584fafd199a754d04c7fe4
blank-hoodie <d.m-2006@mail.ru>
2025-10-25 14:25:43 +0300

    version 1.1

--- pract_1/main.cpp
+++ pract_1/main.cpp
@@ -4,4 +4,11 @@ int main() {
 	using ut = unsigned;
 	ut x = 0;
 	std::cout << a;
+	if (std::cin.eof()) {
+		std::cout << "1";
+		std::cout << "\n";
+	} else if (std::cin.fail()) {
+		std::cerr << "input err";
+		return 1;
+	}
 }
