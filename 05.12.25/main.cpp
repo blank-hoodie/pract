f19d638fe443567a21459eaee5e34200d7f1231c
blank-hoodie <d.m-2006@mail.ru>
2025-12-05 12:31:35 +0300

    live cod

--- 05.12.25/main.cpp
+++ 05.12.25/main.cpp
@@ -1,4 +1,14 @@
-include <iostream>
+#include <iostream>
+
+struct p_t{int x, int y};
+struct IDrow 
+{
+virtual p_t begin() const = 0;
+virtual p_t next() const = 0;
+virtual ~IDrow();
+
+};
 main()
-  {
-  }
+{
+  
+}
